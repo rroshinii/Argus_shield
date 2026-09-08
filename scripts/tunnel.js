aa824abdc685fd69d520552cae94fd27067c2d78
Roshini <roshiniramakrishnan7@gmail.com>
2026-09-08 12:19:56 +0530

    chore: add scripts/tunnel.js for public localtunnel hosting

---
+++ scripts/tunnel.js
@@ -0,0 +1,15 @@
+﻿const localtunnel = require('localtunnel');
+
+(async () => {
+  try {
+    const tunnel = await localtunnel({ port: 8000, local_host: '127.0.0.1' });
+    console.log('PUBLIC_URL:' + tunnel.url);
+    
+    tunnel.on('close', () => console.log('Tunnel closed'));
+    tunnel.on('error', (err) => console.error('Tunnel error:', err));
+    setInterval(() => {}, 10000);
+  } catch (err) {
+    console.error('Failed to start tunnel:', err);
+    process.exit(1);
+  }
+})();
